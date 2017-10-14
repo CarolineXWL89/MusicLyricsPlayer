3bda2272bb75d508336087d7575c314aa4b98847
CarolineXWL89 <carolinexwliu89@hotmail.com>
2017-10-14 00:12:11 -0700

    #1-Initial commit

---
+++ app/src/main/java/com/example/caroline/musiclyricsplayer/MainMusicPlayer.java
@@ -0,0 +1,13 @@
+package com.example.caroline.musiclyricsplayer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainMusicPlayer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_music_player);
+    }
+}
