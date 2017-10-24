c210fc074ed0350dcf956c64d9cf9a1873dee83f
CarolineXWL89 <carolinexwliu89@hotmail.com>
2017-10-24 15:31:34 -0700

    Caroline #4-Website updates

---
+++ app/src/main/java/com/example/caroline/musiclyricsplayer/TitleSpotifyURI.java
@@ -0,0 +1,40 @@
+package com.example.caroline.musiclyricsplayer;
+
+/**
+ * Created by princ on 24/10/2017.
+ */
+
+public class TitleSpotifyURI {
+    private String title,URI;
+    private int start;
+
+    /*
+    Example Google search links to compare
+    https://www.google.com/search?safe=strict&q=i+whistle+a+happy+tune+spotify&oq=i+whistle+a+happy+tune+spotify&gs_l=psy-ab.3...5657.9742.0.9975.8.8.0.0.0.0.284.488.2-2.2.0....0...1.1.64.psy-ab..6.2.487...0j35i39k1j0i67k1j0i22i30k1.0.mc7rWBhsHzs
+    https://www.google.com/search?safe=strict&source=hp&q=don%27t+stop+me+now+spotify&oq=don%27t+stop+me+now+spotify&gs_l=psy-ab.3...4676.10661.0.10906.32.22.1.0.0.0.411.2824.2-8j0j2.10.0....0...1.1.64.psy-ab..21.11.2828.0..0j35i39k1j0i131k1j0i20i264k1j0i67k1j0i10i67k1j0i22i30k1.0.t05TmiK9U14
+    https://www.google.com/search?safe=strict&source=hp&q=think+of+me+spotify
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getURI() {
+        return URI;
+    }
+
+    public void setURI(String URI) {
+        this.URI = URI;
+    }
+
+    public int getStart() {
+        return start;
+    }
+
+    public void setStart(int start) {
+        this.start = start;
+    }
+}
--- app/src/main/java/com/example/caroline/musiclyricsplayer/URLtoSongObj.java
+++ app/src/main/java/com/example/caroline/musiclyricsplayer/URLtoSongObj.java
@@ -6,7 +6,7 @@ import java.net.URL;
  * Created by maylisw on 10/19/17.
  */
 
-//link is to lyrics url is to search result
+//link is to lyrics url is to search result --> URL is from lyrics.com
 
 public class URLtoSongObj {
     private String url, title, artist;
