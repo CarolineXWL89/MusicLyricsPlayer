9d24c8faa68b3b804c1415e402e43999c4006d9d
CarolineXWL89 <carolinexwliu89@hotmail.com>
2017-10-24 17:19:34 -0700

    Caroline #5-HTMLReader done --> reads through given HTML code to get components

---
+++ app/src/main/java/com/example/caroline/musiclyricsplayer/HTMLReader.java
@@ -0,0 +1,56 @@
+package com.example.caroline.musiclyricsplayer;
+
+import java.util.ArrayList;
+
+/**
+ * Created by princ on 24/10/2017.
+ * Runs through the HTML code and finds the div classes to access title, composer, and URL for lyrics page
+ */
+
+public class HTMLReader {
+    private String title, artist, HTMLCode, lyricsURL;
+    private ArrayList<Integer> positions = new ArrayList<>();
+    //private int firstSearched;
+
+    public HTMLReader(String HTMLCode){
+        this.HTMLCode = HTMLCode;
+    }
+
+    public ArrayList<Integer> findFirstOccurances(){
+        int firstSearchedBeforeTitle = HTMLCode.indexOf("<a href=\"/lyric");
+        positions.add(firstSearchedBeforeTitle);
+        int firstBeforeArtist = HTMLCode.indexOf("artists\"><a href=\"artist/");
+        positions.add(firstBeforeArtist);
+        int firstBeforeLyricLink = HTMLCode.indexOf("lyric-body\" onclick=\"location.href='https://www.lyrics.com/");
+        positions.add(firstBeforeLyricLink);
+        return positions;
+    }
+
+
+    public String findTitle(){
+        int firstTitle = positions.get(0);
+        String shortenedSearch = HTMLCode.substring(firstTitle);
+        int sideCarrotTitleStart = shortenedSearch.indexOf(">");
+        int sideCarrotTitleEnd = shortenedSearch.indexOf("<");
+        title = shortenedSearch.substring(sideCarrotTitleStart + 1, sideCarrotTitleStart);
+        return title;
+    }
+
+    public String findComposer(){
+        int firstComposer = positions.get(1);
+        String shortenedSearch = HTMLCode.substring(firstComposer);
+        int sideCarrotTitleStart = shortenedSearch.indexOf(">");
+        int sideCarrotTitleEnd = shortenedSearch.indexOf("<");
+        artist = shortenedSearch.substring(sideCarrotTitleStart + 1, sideCarrotTitleStart);
+        return artist;
+    }
+
+    public String findLyricsURL(){
+        int firstLyricLink = positions.get(2);
+        String shortenedSearch = HTMLCode.substring(firstLyricLink);
+        int sideCarrotTitleStart = shortenedSearch.indexOf(">");
+        int sideCarrotTitleEnd = shortenedSearch.indexOf("<");
+        lyricsURL = shortenedSearch.substring(sideCarrotTitleStart + 1, sideCarrotTitleStart);
+        return lyricsURL;
+    }
+}
--- app/src/main/java/com/example/caroline/musiclyricsplayer/TitleSpotifyURI.java
+++ app/src/main/java/com/example/caroline/musiclyricsplayer/TitleSpotifyURI.java
@@ -12,7 +12,8 @@ public class TitleSpotifyURI {
     Example Google search links to compare
     https://www.google.com/search?safe=strict&q=i+whistle+a+happy+tune+spotify&oq=i+whistle+a+happy+tune+spotify&gs_l=psy-ab.3...5657.9742.0.9975.8.8.0.0.0.0.284.488.2-2.2.0....0...1.1.64.psy-ab..6.2.487...0j35i39k1j0i67k1j0i22i30k1.0.mc7rWBhsHzs
     https://www.google.com/search?safe=strict&source=hp&q=don%27t+stop+me+now+spotify&oq=don%27t+stop+me+now+spotify&gs_l=psy-ab.3...4676.10661.0.10906.32.22.1.0.0.0.411.2824.2-8j0j2.10.0....0...1.1.64.psy-ab..21.11.2828.0..0j35i39k1j0i131k1j0i20i264k1j0i67k1j0i10i67k1j0i22i30k1.0.t05TmiK9U14
-    https://www.google.com/search?safe=strict&source=hp&q=think+of+me+spotify
+    https://www.google.com/search?safe=strict&source=hp&q=think+of+me+spotify --> directly into the URL bar;
+    ^^top two are obtained when gotten from search bar directly
      */
     public String getTitle() {
         return title;
